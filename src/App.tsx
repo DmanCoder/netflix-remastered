057e1eff1685dd81655c5564f97b05dbfd17a75d
DmanCoder <ktdenis.1@live.com>
2021-01-27 13:44:22 +1000

    Test

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface IAppProps {}
+
+const App: React.FC<IAppProps> = ({}) => {
+  return (
+    <div>
+      <h2>TESTING</h2>
+    </div>
+  );
+};
+
+export default App;
