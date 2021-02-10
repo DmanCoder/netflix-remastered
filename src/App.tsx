8d658b4ebdbc405e26e22ab0e7332f6fd09aa945
DmanCoder <ktdenis.1@live.com>
2021-02-10 14:34:42 +1000

    Setup base api

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+// Init
+import { dbAPI, REACT_APP_TMBD_KEY } from './api/init';
 
 interface IAppProps {}
 
 const App: React.FC<IAppProps> = ({}) => {
+  useEffect(() => {
+    console.log(REACT_APP_TMBD_KEY);
+    dbAPI.get(`/discover/movie?api_key=${REACT_APP_TMBD_KEY}`).then((res) => {
+      console.log(res);
+    });
+  }, []);
+
   return <div></div>;
 };
 
--- src/api/init.ts
+++ src/api/init.ts
@@ -1,10 +1,14 @@
 import axios from 'axios';
 
-const dbURL = 'https://api.themoviedb.org/3';
+// Base URL string
+const dbURL: string = 'https://api.themoviedb.org/3';
+
+// Environment variables
+const { REACT_APP_TMBD_KEY } = process.env;
 
 // AXIOS INSTANCE: BASE URL FOR THE MOVIE DATA BASE
 const dbAPI = axios.create({
   baseURL: dbURL,
 });
 
-export { dbAPI, dbURL };
+export { dbAPI, dbURL, REACT_APP_TMBD_KEY };
