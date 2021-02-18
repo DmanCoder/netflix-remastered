0dbdd43e4542582403f423b83ef65fe8ff15ebff
DmanCoder <ktdenis.1@live.com>
2021-02-18 15:49:02 +1000

    Added checks

--- src/validations/isEmptyVAL.ts
+++ src/validations/isEmptyVAL.ts
@@ -1,4 +1,4 @@
-const isEmptyVAL = (value) =>
+const isEmptyVAL = (value: any): any =>
   value === undefined ||
   value === null ||
   (typeof value === 'object' && Object.keys(value).length === 0) ||
